5777a70cac69fef20476c1e35afbfaef3594b278
Adonis De Paula <adonsfco@gmail.com>
2023-03-21 21:00:16 -0400

    [ref] Add a message to show that the db is up.

--- API/index.mjs
+++ API/index.mjs
@@ -1,15 +1,35 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
 import {getFullProfiles,getSingleProfile, getProfileList} from "./Get/profiles.mjs";
+import {setNewProfile} from "./Set/profiles.mjs"
 
 dotenv.config();
 const port = process.env.SERVER_PORT;
 
 const app = express();
+app.use(bodyParser.json())
 
-app.use('/get/FullProfiles/offset/:offset/page/:page',getFullProfiles,);
-app.use('/get/ProfileList/offset/:offset/page/:page',getProfileList);
-app.use('/profile/:id',getSingleProfile); 
+
+//---------------Get--->
+//Profile
+app.get('/get/FullProfiles/offset/:offset/page/:page',getFullProfiles,);
+app.get('/get/ProfileList/offset/:offset/page/:page',getProfileList);
+app.get('/profile/:id',getSingleProfile); 
+
+//Programs
+
+//Scripts 
+
+//---------------Set--->
+
+//Profile
+    app.post('/set/profile',setNewProfile);
+
+//Programs
+
+//Scripts 
+
 
 
 
