2d7e25b3df4803d1e71d43cb0014bd8b3ce6481c
Adonis De Paula <adonsfco@gmail.com>
2023-03-25 18:59:40 -0400

    [feat] Add update profile.

--- API/Put/profiles.mjs
+++ API/Put/profiles.mjs
@@ -1,25 +1,56 @@
 import { ObjectId } from "mongodb";
 import db from "../Database/db.mjs";
 
-  async function updateProfile(req, res) {
-    const profiles = db.collection("Profiles");
+async function updateProfile(req, res) {
+  const profiles = db.collection("Profiles");
+  const { id } = req.params;
+  const allowedFields = ["name", "logo", "agent", "addedBy", "date", "changeDate"];
+  const notAllowedFields =["globalPrograms", "uniquePrograms", "globalScripts", "uniqueScripts"]
+  if (!id) {
+    return res.status(204).send("id is not present.");
+  }
 
-  try {
-    //Check if the parameter of the profile exists.
-    if (!req.body.id) {
-      res.status(204).send("id is not present.");
-      return;
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).send("Request body is empty or missing.");
+  }
+
+  const updateObj = {};
+  for (const field in req.body) {
+
+    if (allowedFields.includes(field)) {
+        updateObj.$set = updateObj.$set || {};
+        updateObj.$set[field] = req.body[field];
+      }
+       else {
+        if(notAllowedFields.includes(field))
+        {
+       
+          return res.status(400).send(`Invalid parameter: ${field}
+          The parameters ${notAllowedFields} are valid however they needs to be update by its correct method.
+          Read the documentation for more details.
+          `);
+        }
+        
+      return res.status(400).send(`Invalid parameter: ${field}`);
     }
-    const id = new ObjectId(req.body.id);
-    //Determine that the id exists on the database.
-    const profileToUpdate = profiles.findOne({ _id: id }).toArray()
-    if (profileToUpdate.length === 1) {
-      console.log(profileToUpdate);
+  }
+  
 
+  const profileToUpdate = await profiles.findOne({ _id: new ObjectId(id) });
+  if (!profileToUpdate) {
+    return res.status(404).send({ message: "Profile not found." });
+  }
+  //Change the time that was changed
+  const currentDate = new Date();
+  updateObj.$set.changeDate = currentDate;
 
-
-    }
-  } catch (error) {}
+  await profiles.updateOne({ _id:new ObjectId(id) }, updateObj);
+  return res.status(200).send({
+    message: "Profile updated successfully.",
+    success: true,
+    updates: updateObj.$set,
+  });
 }
+
 
 export { updateProfile };
